69e046f74583324856c286aa971c0d2402fc81b6
healthy27 <healthy27@kangwon.ac.kr>
2026-06-10 20:00:23 +0900

    Set up assignment 2 React project

---
+++ assignment-2/src/constants/todo.js
@@ -0,0 +1,13 @@
+export const TODO_STORAGE_KEY = 'assignment-2.todos'
+
+export const TODO_FILTERS = {
+  ALL: 'all',
+  ACTIVE: 'active',
+  COMPLETED: 'completed',
+}
+
+export const TODO_FILTER_LABELS = {
+  [TODO_FILTERS.ALL]: '전체',
+  [TODO_FILTERS.ACTIVE]: '진행 중',
+  [TODO_FILTERS.COMPLETED]: '완료',
+}
